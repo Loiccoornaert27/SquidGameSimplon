467ce8f8665da2a91d2f2ad9a569328782906136
Loic Coornaert <loic.coornaert27@gmail.com>
2021-12-16 11:42:40 +0100

    Update boutton pair et impair

--- script/main.js
+++ script/main.js
@@ -1,4 +1,15 @@
 "use strict";
+let choixUser; //Va stocker si le choix est pair ou impaire
+const btnPair = document.getElementById("btnPair");
+const btnImpair = document.getElementById("btnImpair");
+function pairClick() {
+    return "pair";
+}
+btnPair === null || btnPair === void 0 ? void 0 : btnPair.addEventListener("click", pairClick);
+function impairClick() {
+    return "impair";
+}
+btnImpair === null || btnImpair === void 0 ? void 0 : btnImpair.addEventListener("click", impairClick);
 /*****Fonction qui décide qui gagne et qui perd ********/
 //nbBilles : le nombre de billes choisies
 //choixUser : pair ou impair
@@ -7,18 +18,22 @@
 function checkResult(nbBilles, choixUser, nbPari, isJoueur) {
     if (nbBilles % 2 == 0) {
         if (choixUser == "pair" && isJoueur || choixUser == "impair" && !isJoueur) {
-            console.log('tu gagnes ' + nbPari + ' billes');
+            console.log('tu gagnes ' + nbPari + ' billes'); //A changer en inner HTML
+            return nbPari;
         }
         else if (choixUser == "impair" && isJoueur || choixUser == "pair" && !isJoueur) {
-            console.log('tu perds ' + nbPari + ' billes');
+            console.log('tu perds ' + nbPari + ' billes'); //A changer en inner HTML
+            return nbPari * (-1);
         }
     }
     else if (nbBilles % 2 != 0) {
         if (choixUser == "impair" && isJoueur || choixUser == "pair" && !isJoueur) {
-            console.log('tu gagnes ' + nbPari + ' billes');
+            console.log('tu gagnes ' + nbPari + ' billes'); //A changer en inner HTML
+            return nbPari;
         }
         else if (choixUser == "pair" && isJoueur || choixUser == "impair" && !isJoueur) {
-            console.log('tu perds ' + nbPari + ' billes');
+            console.log('tu perds ' + nbPari + ' billes'); //A changer en inner HTML
+            return nbPari * (-1);
         }
     }
 }
