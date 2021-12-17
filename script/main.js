40d8db118b6fb7e9a115d28261bbd06b41cabe78
n00gat <94345886+n00gat@users.noreply.github.com>
2021-12-17 10:11:39 +0100

    fix

--- script/main.js
+++ script/main.js
@@ -55,19 +55,31 @@ function checkResult(nbBilles, choixUser, nbPari, isJoueur) {
         }
     }
 }
+<<<<<<< Updated upstream
 //Fonction qui désigne aléatoirement qui va jouer en premier,
 //playerTurn : tour du joueur (true) ou tour IA (false)
+=======
+//Fonction qui désigne aléatoirement qui va jouer en premier, introduit la variable playerTurn qui définit si c'est le tour du joueur ou non
+let playerTurn = true;
+>>>>>>> Stashed changes
 function whoPlayFirst(playerTurn) {
     playerTurn = Math.random() < 0.5;
     return playerTurn;
 }
 playerTurn = whoPlayFirst(playerTurn);
 console.log("playerTurn is " + playerTurn);
+<<<<<<< Updated upstream
 //Pari de l'IA
 //Génère un nombre de billes pariées par l'IA
 //numMarblesAI : nombres de billes de l'IA
 //numMarblesPlayer : nombres de billes du joueur
 //marblesBetIA : pari de l'IA
+=======
+//Initialisation du stock de billes
+let numMarblesPlayer = 10;
+let numMarblesAI = 10;
+//Pari de l'IA
+>>>>>>> Stashed changes
 let marblesBetAI;
 function initBetAI(numMarblesAI, numMarblesPlayer) {
     let min = Math.ceil(1);
@@ -84,7 +96,15 @@ function initBetAI(numMarblesAI, numMarblesPlayer) {
     return Math.floor(Math.random() * (max - min)) + min;
 }
 marblesBetAI = initBetAI(numMarblesAI, numMarblesPlayer);
+<<<<<<< Updated upstream
 //Fonction d'addition/soustraction des billes au stock en fonction de *Fonction qui décide qui gagne et qui perd *
+=======
+//Boucle de jeu
+while (numMarblesPlayer || numMarblesAI > 0) {
+    (playerTurn == true ? false : true);
+}
+//Fonction d'ajout ou de retrait  
+>>>>>>> Stashed changes
 function addRemoveMarbles(numMarblesAI, numMarblesPlayer, nbPari) {
     if (playerTurn == true) {
         numMarblesPlayer += nbPari;
@@ -94,8 +114,11 @@ function addRemoveMarbles(numMarblesAI, numMarblesPlayer, nbPari) {
         numMarblesAI += nbPari;
         numMarblesPlayer -= nbPari;
     }
+<<<<<<< Updated upstream
 }
 //Boucle de jeu
 while (numMarblesPlayer || numMarblesAI > 0) {
     (playerTurn == true ? false : true);
+=======
+>>>>>>> Stashed changes
 }
