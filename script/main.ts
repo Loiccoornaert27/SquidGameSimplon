d5a76d179d0a4aa5a2d0a5167af6cb2ade01f4ba
Kamoullox <boucetta.kamel62@gmail.com>
2021-12-17 12:21:39 +0100

    Add transition between different screen

--- script/main.ts
+++ script/main.ts
@@ -21,6 +21,13 @@ closeWindowRules.addEventListener("click",closeRules);
 
 
 function closeRules(){
+// Cache l'écran des règles et Affiche l'écran de jeu
+function closeRules() {
+    const screenRules = document.getElementsByClassName("screen-rules")[0];
+    const screenGame = document.getElementsByClassName("screen-game")[0];
+
+    generateMarblesPlayerImage();
+
     screenRules.setAttribute("style", "display: none");
     screenGame.setAttribute("style", "display: block");
 }
@@ -46,11 +53,89 @@ function impairClick(){
 }
 
 btnImpair.addEventListener("click",impairClick);
+// Génere les billes dans la mains du joueur en fonction de la variable numMarblesPlayer
+function generateMarblesPlayerImage() {
+    const playerMarbles = document.getElementsByClassName("player-marbles")[0];
+
+    for (let i = 1; i <= numMarblesPlayer; i++) {
+        // Création de l'image
+        let img = document.createElement('img');
+        img.src = "./image/bille.png";
+        img.classList.add("marble");
+
+        img.addEventListener("click", () => {confirmationMarblesPLayer(i);});
+
+        playerMarbles.appendChild(img);
+        console.log("Nouvelle image généré !")
+    }
+}
+
+function confirmationMarblesPLayer(numberOfMarble : number){
+    marblesBetPlayer = numberOfMarble;
+    displayConfirmationButton();
+}
+
+// Fait apparaitre les bouttons oui/non
+function displayConfirmationButton(){
+    let confirmationBtn = document.querySelector(".validationButton") as Element;
+    confirmationBtn.setAttribute("style", "display: flex");
+
+    let noBtn = document.querySelector('.no') as Element;
+    noBtn.addEventListener("click", noButton);
+
+    let yesBtn = document.querySelector('.yes') as Element;
+    yesBtn.addEventListener("click", yesButton);
+
+    updateTextMiddle(`Voulez vous pariez ${marblesBetPlayer} ${marblesBetPlayer === 1 ? "bille" : "billes"} ? `);
+}
+
+// Fait disparaitre les bouttons oui/non
+function noButton(){
+    marblesBetPlayer = 0;
+
+    let confirmationBtn = document.querySelector(".validationButton") as Element;
+    confirmationBtn.setAttribute("style", "display: none");
+
+    updateTextMiddle(`Choisissez le nombre de billes à parier`);
+}
+
+// Enleve les billes joueur et fait apparaitre les bouton pair/impair
+function yesButton(){
+    let confirmationBtn = document.querySelector(".validationButton") as Element;
+    let marblesPlayers = document.querySelector('.player-marbles') as Element; 
+
+    confirmationBtn.setAttribute("style", "display: none");
+    marblesPlayers.setAttribute("style", "display: none");
+
+    let playerChoice = document.querySelector('.player-choice') as Element;
+    playerChoice.setAttribute("style", "display: flex")
+
+    updateTextMiddle(`Choisissez pair ou impair`);
+}
+
+// Change le texte du milieu
+function updateTextMiddle(str : string){
+    let displayTxt = document.querySelector(".display-text p") as Element;
+    displayTxt.innerHTML = str;
+}
+
+
+function pairClick() {
+    return "pair";
+}
+
+btnPair?.addEventListener("click", pairClick);
+
+function impairClick() {
+    return "impair";
+}
+
+btnImpair?.addEventListener("click", impairClick);
 
 /*Fonction qui permet de déterminer si l'IA choisit pair ou impair*/
-function aiChoose(){
-    let num= Math.floor(Math.random()*10);
-    if(num%2 == 0){
+function aiChoose() {
+    let num = Math.floor(Math.random() * 10);
+    if (num % 2 == 0) {
         return "pair";
     }
     else {
@@ -98,8 +183,8 @@ console.log("playerTurn is "+playerTurn)
 
 
 //Pari de l'IA
-let marblesBetIA:number;
-function initBetIA(numMarblesIA:number,numMarblesPlayer: number){
+let marblesBetIA: number;
+function initBetIA(numMarblesIA: number, numMarblesPlayer: number) {
     let min = 1;
     //20% de chance de parier plus que le stock de billes du joueur
     let chanceToBetMore=Math.random();
